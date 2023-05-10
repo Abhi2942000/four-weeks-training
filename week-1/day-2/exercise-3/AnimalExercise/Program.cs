925602347bf83bba9593213dd06d3d70c50146d0
Abhi2942000 <bhosaleabhishek777@gmail.com>
2023-05-10 14:54:20 +0530

    added

--- week-1/day-2/exercise-3/AnimalExercise/Program.cs
+++ week-1/day-2/exercise-3/AnimalExercise/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace AnimalExercise
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+abstract class Animal
 {
-    internal class Program
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public abstract void MakeSound();
+}
+class Dog : Animal, IMovable
+{
+    public override void MakeSound()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Woof");
+    }
+    public void Move()
+    {
+        Console.WriteLine("Running on four legs");
+    }
+}
+class Cat : Animal, IMovable
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+    public void Move()
+    {
+        Console.WriteLine("Jumping and climbing");
+    }
+}
+interface IMovable
+{
+    void Move();
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal>();
+        animals.Add(new Dog() { Name = "Buddy", Age = 2 });
+        animals.Add(new Cat() { Name = "Whiskers", Age = 1 });
+        foreach (Animal animal in animals)
         {
-            // Create a list of Animal objects, add Dog and Cat instances, and call their methods
+            Console.WriteLine($"Name: {animal.Name}, Age: {animal.Age}");
+            animal.MakeSound();
+            if (animal is IMovable movable)
+            {
+                movable.Move();
+            }
         }
     }
 }
