ac1321cd34944f5a069c8172e3a00e1ad1921d22
Abhi2942000 <bhosaleabhishek777@gmail.com>
2023-05-10 15:15:22 +0530

    factorial added

--- week-1/day-3/exercise-1/FactorialApp/Program.cs
+++ week-1/day-3/exercise-1/FactorialApp/Program.cs
@@ -6,15 +6,18 @@
         {
             Console.Write("Enter a number: ");
             int number = int.Parse(Console.ReadLine());
-
             long factorial = CalculateFactorial(number);
-
             Console.WriteLine($"The factorial of {number} is: {factorial}");
+            Console.ReadLine();
         }
-
         public static long CalculateFactorial(int number)
         {
-            throw new NotImplementedException();
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
         }
     }
 }
