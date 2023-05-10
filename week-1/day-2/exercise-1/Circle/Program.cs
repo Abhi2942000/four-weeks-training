4d820a2d1947e9bccd8f4bb65e2ab3f2a75efffb
Abhi2942000 <bhosaleabhishek777@gmail.com>
2023-05-10 14:23:25 +0530

    circle exercise complete

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Circle
+﻿// See https://aka.ms/new-console-template for more information
+namespace AreaOfCircleUsingMethod
 {
-    internal class Program
+    class Circle
     {
-        static void Main(string[] args)
+        public void GetArea(int r)
         {
-            // Create a Circle object and display its area and circumference
+            double cArea;
+            cArea = 3.14 * r * r;
+            Console.WriteLine("Area Of Circle {0} ", cArea);
         }
+
+        public void GetCircumference(int r)
+        {
+            double cCircumference;
+            cCircumference = 3.14 * r * r;
+            Console.WriteLine("cCircumference Of Circle {0} ", cCircumference);
+        }
+
+    }
+
+    class Program
+    {
+        static public void Main()
+        {
+            Circle c = new Circle();
+            c.GetArea(2);
+            c.GetCircumference(8);
+
+        }
+
     }
 }
