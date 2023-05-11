be925501ac61d05fd2af5277373c9e4eeeedb693
Abhi2942000 <bhosaleabhishek777@gmail.com>
2023-05-11 16:26:10 +0530

    Linuq To Object Added

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -11,17 +11,21 @@
                 new Person { Name = "Mark", Age = 28, Country = "USA" },
                 new Person { Name = "Emily", Age = 22, Country = "Australia" }
             };
-
-
+            int filterAge = 20;
+            var filteredPeople = people.Where(p => p.Age > filterAge);
+            var sortedPeople = filteredPeople.OrderBy(p => p.Name);
+            var projectedPeople = sortedPeople.Select(p => new { p.Name, p.Country });
+            foreach (var person in projectedPeople)
+            {
+                Console.WriteLine($"Name: {person.Name}, Country: {person.Country}");
+            }
             //Write queries using LINQ for following operations
             //1. Get all people from USA
             //2. Get all people above 30
             //3. Sort people by name
             //4. Project/Select only Name and Country of all people
-
         }
     }
-
     public class Person
     {
         public string Name { get; set; }
@@ -29,3 +33,10 @@
         public string Country { get; set; }
     }
 }
+
+
+
+
+
+
+
