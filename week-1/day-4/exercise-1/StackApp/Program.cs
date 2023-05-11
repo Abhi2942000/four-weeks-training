6dfc17a8f8790ccd59efb8892d7b4541f3c38927
Abhi2942000 <bhosaleabhishek777@gmail.com>
2023-05-11 15:53:28 +0530

    Stack T Added

--- week-1/day-4/exercise-1/StackApp/Program.cs
+++ week-1/day-4/exercise-1/StackApp/Program.cs
@@ -2,7 +2,17 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private class Abhi
+        {
+            public int value;
+
+            public Abhi(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        public static void Main(string[] args)
         {
             //ICustomStack<int> intStack = new CustomStack<int>();
             //intStack.Push(1);
@@ -11,6 +21,94 @@
             //Console.WriteLine(intStack.Pop()); // Output: 3
             //Console.WriteLine(intStack.Pop()); // Output: 2
             //Console.WriteLine(intStack.IsEmpty()); // Output: False
+
+
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Pop());
+
+
+
+            CustomStack<string> stack2 = new CustomStack<string>();
+            stack2.Push("aman");
+            stack2.Push("hrithik");
+            stack2.Push("rahul");
+            stack2.Push("sanu");
+
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.IsEmpty());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.IsEmpty());
+
+
+
+            CustomStack<Abhi> stack3 = new CustomStack<Abhi>();
+            stack3.Push(new Abhi(1));
+            stack3.Push(new Abhi(2));
+            stack3.Push(new Abhi(3));
+            stack3.Push(new Abhi(4));
+            stack3.Push(new Abhi(5));
+            stack3.Push(new Abhi(6));
+
+
+
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.IsEmpty());
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.IsEmpty());
+
+
+            Console.ReadLine();
         }
+    }
+
+    public class CustomStack<T>
+    {
+        private List<T> list;
+        private int size;
+
+        public CustomStack()
+        {
+            list = new List<T>();
+            size = 0;
+        }
+
+        public void Push(T item)
+        {
+            list.Add(item);
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (this.IsEmpty())
+            {
+                throw new IndexOutOfRangeException();
+            }
+            T value = list[size - 1];
+            list.RemoveAt(size - 1);
+            size--;
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
     }
 }
